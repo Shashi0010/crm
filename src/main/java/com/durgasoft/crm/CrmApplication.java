ebd0e3856334f2204cd27feb1502181ef0e5e367
Shashi0010 <b.kanthshashi490@gmail.com>
2020-07-13 21:30:51 +0530

    last commit

--- src/main/java/com/durgasoft/crm/CrmApplication.java
+++ src/main/java/com/durgasoft/crm/CrmApplication.java
@@ -2,10 +2,16 @@ package com.durgasoft.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrmApplication {
+public class CrmApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CrmApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(CrmApplication.class, args);
     }
